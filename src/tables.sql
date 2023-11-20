fef07651aacd5e24672a7261e366f0ea1b5a6311
Mamadou <mamadou.thiongane@grenoble-inp.org>
2023-11-20 15:27:49 +0100

    Tables

--- src/tables.sql
+++ src/tables.sql
@@ -46,22 +46,65 @@ CREATE TABLE refuge (
     prix_nuit NUMBER
 );
 
+CREATE TABLE formation (
+    annee_formation INTEGER CHECK (annee_formation > 0) PRIMARY KEY,
+    rang_formation INTEGER CHECK (rang_formation > 0) PRIMARY KEY,
+    nom_formation VARCHAR(30) NOT NULL,
+    date_formation DATE NOT NULL,
+    duree INTEGER NOT NULL CHECK (duree > 0),
+    nb_places_formation INTEGER NOT NULL CHECK (nb_places_formation > 0),
+    infos_formation VARCHAR(30) NOT NULL,
+    prix_formation NUMBER NOT NULL CHECK (prix_formation > 0)
+);
+
+CREATE TABLE reservation_formation (
+    id_adh INTEGER NOT NULL,
+    annee_formation INTEGER NOT NULL,
+    rang_formation INTEGER NOT NULL,
+    rang_la INTEGER NOT NULL,
+    PRIMARY KEY (id_adh, annee_formation, rang_formation),
+    FOREIGN KEY (id_adh) REFERENCES adherent(id_adh),
+    FOREIGN KEY (annee_formation, rang_formation) REFERENCES formation(annee_formation, rang_formation)
+);
+
+CREATE TABLE activite (
+    activite VARCHAR(30) NOT NULL CHECK (activite IN ('randonnée', 'escalade', 'alpinisme', 'spéléologie', 'ski de rando', 'cascade de glace')) PRIMARY KEY,
+);
+
+CREATE TABLE activites_formation (
+    annee_formation INTEGER NOT NULL,
+    rang_formation INTEGER NOT NULL,
+    activite VARCHAR(30) NOT NULL,
+    PRIMARY KEY (annee_formation, rang_formation, activite),
+    FOREIGN KEY (annee_formation, rang_formation) REFERENCES formation(annee_formation, rang_formation),
+    FOREIGN KEY (activite) REFERENCES activite(activite)
+);
+
+CREATE TABLE activites_lot (
+    marque VARCHAR(30) NOT NULL,
+    modele VARCHAR(30) NOT NULL,
+    annee_achat INTEGER NOT NULL CHECK (annee_achat > 0),
+    activite VARCHAR(30) NOT NULL,
+    PRIMARY KEY (marque, modele, annee_achat, activite),
+    FOREIGN KEY (marque, modele, annee_achat) REFERENCES lot(marque, modele, annee_achat),
+    FOREIGN KEY (activite) REFERENCES activite(activite)
+);
 
 CREATE TABLE lot (
     marque VARCHAR(30) NOT NULL,
     modele VARCHAR(30) NOT NULL,
-    annee_achat INTEGER NOT NULL CHECK (annee_achat>0),
-    nb_pieces_lot INTEGER NOT NULL CHECK (nb_pieces_lot>=0),
-    prix_caution INTEGER NOT NULL CHECK (prix_caution>=0),
+    annee_achat INTEGER NOT NULL CHECK (annee_achat > 0),
+    nb_pieces_lot INTEGER NOT NULL CHECK (nb_pieces_lot >= 0),
+    prix_caution INTEGER NOT NULL CHECK (prix_caution >= 0),
     activite VARCHAR(30) NOT NULL,
     infos_materiel VARCHAR(30),
-    annee_peremption INTEGER NOT NULL CHECK (annee_peremption>0),
-    PRIMARY KEY (marque,modele,annee_achat)
+    annee_peremption INTEGER NOT NULL CHECK (annee_peremption > 0),
+    PRIMARY KEY (marque, modele, annee_achat)
 );
 
 CREATE TABLE quantite_materiel (
     nb_pieces_res INTEGER NOT NULL CHECK (nb_pieces_res > 0),
-    nb_pieces_perdues INTEGER NOT NULL CHECK (nb_pieces_perdues >=0)
+    nb_pieces_perdues INTEGER NOT NULL CHECK (nb_pieces_perdues >= 0)
 );
 
 CREATE TABLE categorie (
