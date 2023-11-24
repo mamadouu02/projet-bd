0942560297740e1349a6bb5de035278ec3fde765
rebouluc <150441456+rebouluc@users.noreply.github.com>
2023-11-24 12:38:20 +0100

    Add files via upload

---
+++ src/Lot.java
@@ -0,0 +1,14 @@
+public class Lot {
+    String marque;
+    String modele;
+    int anneAchat;
+
+    int qttDispo;
+
+    public Lot(String marque,String modele,int anneAchat,int qttDispo){
+        this.marque=marque;
+        this.modele=modele;
+        this.anneAchat=anneAchat;
+        this.qttDispo=qttDispo;
+    }
+}
