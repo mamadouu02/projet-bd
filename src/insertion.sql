f294ea208dac28e854edcafdfb63e7b0e82583e6
Ahmedd-b <148774446+Ahmedd-b@users.noreply.github.com>
2023-11-24 12:41:06 +0100

    Update insertion.sql

--- src/insertion.sql
+++ src/insertion.sql
@@ -1,5 +1,9 @@
 /* Table refuge */
-
+Insert Into REFUGE values('Refuge0@gmail.com','Refuge0','0701010101','montagne',DATE '2023-12-12',DATE '2024-05-12',20,20,NULL,'carte-bleue',35);
+Insert Into REFUGE values('Refuge1@gmail.com','Refuge1','0702010101','montagne',DATE '2023-12-15',DATE '2024-05-15',35,35,NULL,'carte-bleue',45);
+Insert Into REFUGE values('Refuge2@gmail.com','Refuge2','0703010101','mer',DATE '2023-12-15',DATE '2024-05-15',40,40,NULL,'carte-bleue',50);
+Insert Into REFUGE values('Refuge3@gmail.com','Refuge3','0704010101','montagne',DATE '2023-12-15',DATE '2024-05-15',40,40,NULL,'carte-bleue',55);
+Insert Into REFUGE values('Refuge4@gmail.com','Refuge4','0705010101','montagne',DATE '2023-12-15',DATE '2024-05-15',50,50,NULL,'carte-bleue',60);
 
 /* Table repas */
 
