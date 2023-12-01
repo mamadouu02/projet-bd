4f97ddfe10e764ac94f7c25a260981dbe79a8ea2
bilal84 <132393334+bilal84@users.noreply.github.com>
2023-12-01 14:51:29 +0100

    Update Interface.java

--- src/Interface.java
+++ src/Interface.java
@@ -669,7 +669,7 @@ public class Interface {
     private void remboursement() {
         try {
             String id_adh_connecte = String.valueOf(user.getIdAdh());
-            String getCoutMaterielAbime = "SELECT (l.prix_caution*q.nb_pieces_perdues) FROM quantite_materiel q JOIN lot l ON q.marque = l.marque AND q.modele = l.modele AND q.annee_achat = l.annee_achat JOIN location_materiel lm ON q.id_res_materiel = lm.id_res_materiel WHERE lm.id_adh = ?";
+            String getCoutMaterielAbime = "SELECT SUM(l.prix_caution*q.nb_pieces_perdues) FROM quantite_materiel q JOIN lot l ON q.marque = l.marque AND q.modele = l.modele AND q.annee_achat = l.annee_achat JOIN location_materiel lm ON q.id_res_materiel = lm.id_res_materiel WHERE lm.id_adh = ?";
             PreparedStatement getCoutMaterielAbimeSQL = conn.prepareStatement(getCoutMaterielAbime);
             getCoutMaterielAbimeSQL.setString(1, id_adh_connecte);
             ResultSet result = getCoutMaterielAbimeSQL.executeQuery();
